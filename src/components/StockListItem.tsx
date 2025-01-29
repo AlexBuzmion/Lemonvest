b827ba1a55cb6a121807293dad88824172207ff7
Alex Buzmion <alex.buzmionii@gmail.com>
2025-01-28 16:28:13 -0800

    render stock list and create stocklist item component

---
+++ src/components/StockListItem.tsx
@@ -0,0 +1,61 @@
+import { View, Text } from "./Themed";
+import { StyleSheet } from "react-native";
+import Colors from "../constants/Colors";
+import { AntDesign } from "@expo/vector-icons";
+
+export default StockListItem;
+
+type Stock = {
+    name: string, 
+    symbol: string,
+    close: string,
+    percent_change: string
+}
+
+type StockListItemProps = {
+    stock: Stock,
+
+}
+
+function StockListItem({stock} : StockListItemProps) {
+    const percentChange = Number.parseFloat(stock.percent_change);
+    const closeChange = Number.parseFloat(stock.close);
+    return (
+        <View style={styles.mainContainer}>
+            <View style={styles.leftContainer}>
+                <Text style= {[styles.symbol]}>
+                    {stock.symbol}{"     "}
+                    <AntDesign name="staro" size={18} color="gray" />
+                </Text>
+                <Text>{stock.name}</Text>
+            </View>
+            <View style={styles.rightContainer}>
+                <Text style={percentChange < 0 ? {color: 'red'} : {color: 'green'}}>
+                    {percentChange > 0 ? '+' : ''}
+                    {percentChange.toFixed(2)}%
+                </Text>
+                <Text>${closeChange.toFixed(2)}</Text>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    mainContainer: {
+        flex: 1,
+        flexDirection: 'row',
+    },
+    symbol: {
+        fontSize: 20, 
+        fontWeight: 'bold',
+        color: Colors.light.tint,
+    },
+    leftContainer: {
+        flex: 1,
+        alignItems: 'flex-start',
+        gap: 5,
+    }, 
+    rightContainer: {
+        alignItems: 'flex-end',
+    },
+});
--- src/constants/Colors.ts
+++ src/constants/Colors.ts
@@ -1,5 +1,5 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
+const tintColorLight = '#3c6000';
+const tintColorDark = '#98b117';
 
 export default {
   light: {
